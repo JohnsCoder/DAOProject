79981ca641d1ca4cebf571d929c4f58351301eac
JohnsCoder <joaopdossantos1515@gmail.com>
2023-07-10 10:57:50 -0300

    feat(DAOFactory, SellerDAOJDBC):implement DAO Factory and JDBC seller dao implementation

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import model.dao.DAOFactory;
+import model.dao.SellerDAO;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -8,6 +10,8 @@ public class Main {
     public static void main(String[] args) {
         Department dep = new Department(1, "books");
         Seller sell = new Seller(2, "Carlos", "carlos@email.com", LocalDate.now(), 4000.0, dep);
+
+        SellerDAO sellerDAO = DAOFactory.createSellerDAO();
         System.out.println(sell);
     }
 }
